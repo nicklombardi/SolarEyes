bc9e120d883b9babb400bac4705bd78deab70265
nicklombardi <nicklombardi@gmail.com>
2013-08-03 20:28:34 -0400

    added navbar and background

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolarEyes::Application.config.secret_token = '677abb51d830f5ca92b3f918133ea4c2907be69cca47232596773734f68aa71bdef376e5c595a851e248859de79c9adf911fb8d6da9830c68e79b82cff344125'
