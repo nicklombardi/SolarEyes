24a933d4f0811ea2db0a869e01469f2030cb01c6
nicklombardi <nicklombardi@gmail.com>
2013-08-05 18:18:17 -0400

    fixed glyphicon font issues

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module SolarEyes
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
