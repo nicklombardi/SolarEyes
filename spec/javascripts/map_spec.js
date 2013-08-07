79ff85a725cb92d078edc4b8afb041419d1184d9
Christine Coulter <christinetcoulter@gmail.com>
2013-08-07 17:34:16 -0400

    Edit route for get_oil_price

--- spec/javascripts/map_spec.js
+++ spec/javascripts/map_spec.js
@@ -1,9 +1,9 @@
 //Test the Object.create method
 describe("Object", function() {
-
   it("should have a create method to duplicate objects", function() {
-    var o = {name: "octopodes"};
+    var o = {name: "team purple octopodes"};
     var o2 = Object.create(o);
-    expect(o2.name).toBe("octopodes");
+    expect(o2.name).toBe("team purple octopodes");
   });
 });
+
