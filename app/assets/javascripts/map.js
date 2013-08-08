b088b2c77b1c4cedcdac6a4194b895ed68ad134d
Christine Coulter <christinetcoulter@gmail.com>
2013-08-08 16:31:46 -0400

    Clean up map.js file. Add comments. Some of them helpful.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,4 @@
-// TODO Get everything inside ONE big object for your app
+// really, really, ridiculously (good looking) big object for our app
 var solarEyes = {
     areasArray: [],
     originalTableData: [],
@@ -6,7 +6,10 @@ var solarEyes = {
     oilBarrels: "",
     oilPrice: "",
     stateInfo: function () {
-        // function to grab json data from #index action in home_controller.rb
+        // function to grab JSON data from #index action in home_controller.rb
+        // calls createAreas() function after receiving JSON data and passes the data as an argument
+        // calls saveOriginalTableData() function after receiving JSON data and passes the data as an argument
+
         var that = this;
         $.ajax({
             url: '/home',
@@ -19,6 +22,10 @@ var solarEyes = {
         });
     },
     getOilPrice: function () {
+        // function to grab JSON data from #get_oil_price action in home_controller.rb
+        // assigns JSON data as oilPrice value
+        // JSON data is latest crude oil stock price from Yahoo finance API
+
         var that = this;
         $.ajax({
             url: '/get_oil_price',
@@ -30,6 +37,9 @@ var solarEyes = {
         });
     },
     enterKey: function () {
+        // function lets user hit 'enter' key to submit desired input value
+        // called by event listener when the map loads
+
         $("#installs").keyup(function (e) {
             if (e.keyCode == 13) {
                 console.log("enter key pressed");
@@ -38,10 +48,9 @@ var solarEyes = {
         });
     },
     mapClick: function () {
-        // TODO declare all variables used in a function
-        // At the top of the function, even if their values are defined later
+        // function for when user clicks on calculate button
+        // called by solarEyes.enterKey() and click event listener when the map loads
 
-        // Also use a single var pattern
         var stateBarrels,
             stateAbbreviation,
             k,
@@ -51,6 +60,8 @@ var solarEyes = {
 
         console.log('map clicked');
 
+        // if else statement to handle invalid user input
+        // assigns user input as value to variable input
         if ($('#installs').val() && ($('#installs').val() > 0)) {
             this.input = $('#installs').val();
         } else {
@@ -60,9 +71,10 @@ var solarEyes = {
         console.log("user input: " + this.input);
         console.log("oil price: " + solarEyes.oilPrice);
 
+        // assigns id attribute of the state that the user clicked on to variable stateAbbreviation
         stateAbbreviation = $(".state-abbreviation").attr("id");
-        // Even do this for iterator increments like 'k'
-        // Calculate conditional max outside of loop
+
+        // assigns value of variable stateBarrels to be equal to the number of barrels of oil (misleadingly named 'description') of the object in originalTableData array whose id is the same as stateAbbreviation
         originalTableDataLength = solarEyes.originalTableData.length;
         for (k = 0; k < originalTableDataLength; k++) {
             if (solarEyes.originalTableData[k].id === stateAbbreviation) {
@@ -70,34 +82,42 @@ var solarEyes = {
             }
         }
 
+        // updates text value of element in DOM
         calculation = this.input * stateBarrels;
         console.log("calculation: " + calculation);
         $('#barrels-display').text(calculation.toFixed(2));
 
+        // updates text value of element in DOM
         oilCalculation = this.input * stateBarrels * solarEyes.oilPrice;
         console.log("oilCalculation: " + oilCalculation);
         $('#oil-value').text(oilCalculation.formatMoney(2, '.', ','));
     },
     area: {
+        // object to clone
         id: "",
         description: ""
     },
     createAreas: function (data) {
+        // function readies areas attribute of var dataProvider in makeMap function
+        // calls makeMap()
+
         var j,
             newArea,
             createAreasDataLength;
 
+        // creates content for each State balloon that the user will see after clicking on a state on the map
         createAreasDataLength = data.length;
         for (j = 0; j < createAreasDataLength; j++) {
             newArea = Object.create(this.area);
             newArea.id = data[j].state_name;
-            // based on Arizona
             newArea.description = '<div class="input-group"><span class="input-group-addon"><span class="glyphicons glyphicon-sun" style="color:#FFCC21"></span> &nbsp;</span><input type="number" class="form-control" autofocus="true" placeholder="installs" id="installs"><span class="input-group-btn"><button class="btn btn-primary" type="button" id="calc-button">Calculate</button></span></div><p style="line-height:"3px"> </p><span class="glyphicon glyphicon-tint"></span> barrels saved annually: <span id="barrels-display"> '+ this.input +' </span><br><span class="glyphicon glyphicon-usd" style="color#00AB01"></span> value: <span id="oil-value"></span><span class="state-abbreviation" id=' + data[j].state_name + '></span>';
             this.areasArray.push(newArea);
         }
         this.makeMap();
     },
     saveOriginalTableData: function (data) {
+        // saves JSON data in originalTableData array
+
         var i,
             newTableData,
             saveOriginalTableDataLength;
@@ -200,220 +220,13 @@ var n = this,
    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
  };
 
-// function to grab json data from #index action in home_controller.rb
-// function stateInfo() {
-//     $.ajax({
-//         url: '/home',
-//         dataType: 'json',
-//         data: 'GET'
-//     }).done(function(data){
-//         console.log(data);
-//         createAreas(data);
-//         saveOriginalTableData(data);
-//     });
-// }
-
-// function getOilPrice() {
-//     $.ajax({
-//         url: '/get_oil_price',
-//         dataType: 'json',
-//         data: 'GET'
-//     }).done(function(data){
-//         console.log("get oil price data: " + data);
-//         oilPrice = data;
-//     });
-// }
-
-// function enterKey(){
-//     $("#installs").keyup(function (e) {
-//         if (e.keyCode == 13) {
-//             console.log("enter key pressed");
-//             mapClick();
-//         }
-//     });
-// }
-
-// function intended for when user clicks on calculate button
-// function mapClick(){
-//     // TODO declare all variables used in a function
-//     // At the top of the function, even if their values are defined later
-
-//     // Also use a single var pattern
-//     var stateBarrels,
-//         stateAbbreviation,
-//         k,
-//         originalTableDataLength,
-//         calculation,
-//         oilCalculation;
-
-//     console.log('map clicked');
-
-//     if ($('#installs').val() && ($('#installs').val() > 0)) {
-//         input = $('#installs').val();
-//     } else {
-//         input = 0;
-//     }
-
-//     console.log("user input: " + input);
-//     console.log("oil price: " + oilPrice);
-
-//     stateAbbreviation = $(".state-abbreviation").attr("id");
-//     // Even do this for iterator increments like 'k'
-//     // Calaculate conditional max outside of loop
-//     originalTableDataLength = originalTableData.length;
-//     for (k = 0; k < originalTableDataLength; k++) {
-//         if (originalTableData[k].id === stateAbbreviation) {
-//             stateBarrels = originalTableData[k].description;
-//         }
-//     }
-
-//     calculation = input * stateBarrels;
-//     $('#barrels-display').text(calculation.toFixed(2));
-
-//     oilCalculation = input * stateBarrels * oilPrice;
-//     $('#oil-value').text(oilCalculation.formatMoney(2, '.', ','));
-
-//     // //circles
-//     // var w = 500;
-//     // var h = 50;
-
-//     // //Data
-//     // var dataset = [ 5, 10, 15, 20, 25 ];
-
-//     // //Create SVG element
-//     // var svg = d3.select("#append_here")
-//     //             .append("svg")
-//     //             .attr("width", 500)
-//     //             .attr("height", 50);
-
-//     // var circles = svg.selectAll("circle")
-//     //     .data(dataset)
-//     //     .enter()
-//     //     .append("circle");
-
-//     // circles.attr("cx", function(d, i) {
-//     //             return (i * 50) + 25;
-//     //         })
-//     //        .attr("cy", h/2)
-//     //        .attr("r", function(d) {
-//     //             return d;
-//     //        });
-
-// }
-
-// object to clone
-// var area = {
-//     id: "",
-//     description: ""
-// };
-
-// function readies areas attribute of var dataProvider in makeMap function
-// function createAreas(data) {
-//     var j,
-//         newArea,
-//         createAreasDataLength;
-
-//     createAreasDataLength = data.length;
-//     for (j = 0; j < createAreasDataLength; j++) {
-//         newArea = Object.create(area);
-//         newArea.id = data[j].state_name;
-//         // based on Arizona
-//         newArea.description = '<div class="input-group"><span class="input-group-addon"><span class="glyphicons glyphicon-sun" style="color:#FFCC21"></span> &nbsp;</span><input type="number" class="form-control" autofocus="true" placeholder="installs" id="installs"><span class="input-group-btn"><button class="btn btn-primary" type="button" id="calc-button">Calculate</button></span></div><p style="line-height:"3px"> </p><span class="glyphicon glyphicon-tint"></span> barrels saved annually: <span id="barrels-display"> '+ input +' </span><br><span class="glyphicon glyphicon-usd" style="color#00AB01"></span> value: <span id="oil-value"></span><span class="state-abbreviation" id=' + data[j].state_name + '></span>';
-//         areasArray.push(newArea);
-//     }
-//     makeMap();
-// }
-
-// function saveOriginalTableData(data) {
-//     var i,
-//         newTableData,
-//         saveOriginalTableDataLength;
-
-//     saveOriginalTableDataLength = data.length;
-//     for (i = 0; i < saveOriginalTableDataLength; i ++) {
-//         newTableData = Object.create(area);
-//         newTableData.id += data[i].state_name;
-//         newTableData.description += data[i].barrels_of_oil_per_year;
-//         originalTableData.push(newTableData);
-//     }
-// }
-
-// content of this function originally inside amCharts.ready function
-// function makeMap(){
-//     var map,
-//         dataProvider;
-
-//      // create AmMap object
-//     map = new AmCharts.AmMap();
-
-//     fitMapToContainer = true;
-//     // set path to images
-//     map.pathToImages = "/assets/";
-
-//     map.balloon.adjustBorderColor = true;
-//     map.balloon.borderColor = "#BDBDBD";
-//     map.balloon.color = "#333333";
-//     map.balloon.fillColor = "#FFFFFF";
-//     map.balloon.cornerRadius = "3";
-//     map.balloon.borderThickness = "1";
-//     map.balloon.fontSize = "20";
-//     map.balloon.horizontalPadding = "5";
-//     map.balloon.textShadowColor = "#FFFFFF";
-//     map.balloon.fillAlpha = "0.8";
-//     map.fontFamily = "HelveticaNeue-Light";
-
-//      create data provider object
-//      mapVar tells the map name of the variable of the map data. You have to
-//      view source of the map file you included in order to find the name of the
-//      variable - it's the very first line after commented lines.
-
-//      getAreasFromMap indicates that amMap should read all the areas available
-//      in the map data and treat them as they are included in your data provider.
-//      in case you don't set it to true, all the areas except listed in data
-//      provider will be treated as unlisted.
-
-//     dataProvider = {
-//         mapVar: AmCharts.maps.usaHigh,
-//         getAreasFromMap:true,
-
-//         areas: areasArray,
-
-//         zoomLevel: 1,
-//         zoomLongitude: 18,
-//         zoomLatitude: 30
-//     };
-
-//     /* create areas settings
-//      * autoZoom set to true means that the map will zoom-in when clicked on the area
-//      * selectedColor indicates color of the clicked area.
-//      */
-//     map.areasSettings = {
-//         autoZoom: true,
-//         descriptionWindowY: 350,
-//         descriptionWindowWidth: 280,
-//         descriptionWindowHeight: 330,
-//         rollOverOutlineColor: "#3277BA",
-//         selectedColor: "#60ABEB"
-//     };
-
-//     // pass data provider to the map object
-//     map.dataProvider = dataProvider;
-
-//     // let's say we want a small map to be displayed, so let's create it
-//     // map.smallMap = new AmCharts.SmallMap();
-
-//     // write the map to container div
-//     map.write("mapdiv");
-
-// }
-
-// add all your code to this method, as this will ensure that page is loaded
+// responsible for page load
 AmCharts.ready(function() {
 
-    // call stateInfo function
     solarEyes.stateInfo();
     solarEyes.getOilPrice();
 
+    // event listeners
     $('#mapdiv').on('click', '#calc-button', solarEyes.mapClick);
     $('#mapdiv').on('keyup', solarEyes.enterKey);
 
