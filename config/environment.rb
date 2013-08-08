0eacaabe2974ba8bd3fcc0e27a100c540843b493
nicklombardi <nicklombardi@gmail.com>
2013-08-08 10:35:09 -0400

    adding SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SolarEyes::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app17329511@heroku.com'],
+  :password       => ENV['3hqxdrub'],
+  :domain         => 'solareyes.herokuapp.com',
+  :enable_starttls_auto => true
+}
