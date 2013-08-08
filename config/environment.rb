a1b24bb530616c87748a2dbd27afc5d8b48c9ad1
nicklombardi <nicklombardi@gmail.com>
2013-08-08 14:30:29 -0400

    undid todo refactoring

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SolarEyes::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.sendgrid.net",
-  :port => 25,
-  :domain => "solarey.es",
-  :authentication => :plain,
-  :user_name => "app17329511@heroku.com",
-  :password => "3hqxdrub"
-}
